0fc212b2b9adab9888dbfa0cb9ceabcd11cef4ba
fanrenqing <qing7454@163.com>
2018-01-10 17:18:36 +0800

    第一次提交

---
+++ hello/src/main/java/com/hello/World.java
@@ -0,0 +1,8 @@
+package com.hello;
+
+public class World {
+
+    public static String name() {
+        return "world";
+    }
+}
---
+++ hello/src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module com.hello {
+    requires java.base;
+    exports com.hello;
+}
